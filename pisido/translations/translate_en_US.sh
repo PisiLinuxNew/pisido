1d74286d4477ea8df2bc7804e16e82e43c8703c0
Harun Gültekin <hrngultekin@gmail.com>
2015-09-06 23:04:32 +0300

    delete files

--- pisido/translations/translate_en_US.sh
+++
@@ -1,4 +0,0 @@
-# cd ~/calismalar/pisido
-lupdate ../pisido.pro -ts pisido_en_US.ts -no-obsolete
-linguist pisido_en_US.ts
-lrelease -compress pisido_en_US.ts -qm pisido_en_US.qm
--- pisido/translations/translate_tr_TR.sh
+++
@@ -1,4 +0,0 @@
-# cd ~/calismalar/pisido
-lupdate ../pisido.pro -ts pisido_tr_TR.ts -no-obsolete
-linguist pisido_tr_TR.ts
-lrelease -compress pisido_tr_TR.ts -qm pisido_tr_TR.qm
